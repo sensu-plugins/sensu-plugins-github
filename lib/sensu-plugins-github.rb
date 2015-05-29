0ed1c0dc2d60e78390d5392143b41fad0d149caf
Matt Jones <mattjones@yieldbot.com>
2015-05-29 10:13:14 -0400

    working on modules

--- lib/sensu-plugins-github.rb
+++ lib/sensu-plugins-github.rb
@@ -2,6 +2,10 @@ require 'sensu-plugins-github/version'
 require 'sensu-plugins-github/auth'
 require 'sensu-plugins-github/api'
 
+require 'sensu-plugin/check/cli'
+require 'rest-client'
+require 'json'
+
 # Load the defaults
 
 #
--- lib/sensu-plugins-github/api.rb
+++ lib/sensu-plugins-github/api.rb
@@ -2,29 +2,31 @@
 # Issue Api Request
 #
 module SensuPluginsGithub
-  # Issue the api request
-  #
-  # @param [String] resource
-  #
-  def api_request(resource) #rubocop:disable all
-    endpoint = config[:api] + resource
-    request = RestClient::Resource.new(endpoint, timeout: config[:timeout])
-    headers = {}
-    headers[:Authorization] = "token #{config[:token]}" if config[:token]
-    JSON.parse(request.get(headers), symbolize_names: true)
-  rescue RestClient::ResourceNotFound
-    warning "Resource not found (or not accessible): #{resource}"
-  rescue Errno::ECONNREFUSED
-    warning 'Connection refused'
-  rescue RestClient::RequestFailed => e
-    # #YELLOW Better handle github rate limiting case
-    # (with data from e.response.headers)
-    warning "Request failed: #{e.inspect}"
-  rescue RestClient::RequestTimeout
-    warning 'Connection timed out'
-  rescue RestClient::Unauthorized
-    warning 'Missing or incorrect Github API credentials'
-  rescue JSON::ParserError
-    warning 'Github API returned invalid JSON'
+  module API
+    # Issue the api request
+    #
+    # @param [String] resource
+    #
+    def self.api_request(resource, api, token) #rubocop:disable all
+      endpoint = api + resource
+      request = RestClient::Resource.new(endpoint, timeout: 30)
+      headers = {}
+      headers[:Authorization] = "token #{ token }"
+      JSON.parse(request.get(headers), symbolize_names: true)
+    rescue RestClient::ResourceNotFound
+      warning "Resource not found (or not accessible): #{resource}"
+    rescue Errno::ECONNREFUSED
+      warning 'Connection refused'
+    rescue RestClient::RequestFailed => e
+      # #YELLOW Better handle github rate limiting case
+      # (with data from e.response.headers)
+      warning "Request failed: #{e.inspect}"
+    rescue RestClient::RequestTimeout
+      warning 'Connection timed out'
+    rescue RestClient::Unauthorized
+      warning 'Missing or incorrect Github API credentials'
+    rescue JSON::ParserError
+      warning 'Github API returned invalid JSON'
+    end
   end
 end
