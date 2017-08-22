67541d65d7d92cc95bdce154634f496f3b91758d
Tim Smith <tsmith@chef.io>
2017-08-22 09:38:13 -0700

    Fix the auth logic to return a string w/o newlines
    The previous method returned an array with new lines which didn't work in any of the plugins
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- lib/sensu-plugins-github/auth.rb
+++ lib/sensu-plugins-github/auth.rb
@@ -4,9 +4,7 @@
 module SensuPluginsGithub
   module Auth
     def self.acquire_git_token
-      File.readlines(File.expand_path('~/.ssh/git_token')).each do |line|
-        @github_token = line
-      end
+      File.readlines(File.expand_path('~/.ssh/git_token'))[0]
     end
   end
 end
